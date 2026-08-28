1a6fcbf0ce247885535f6ff742d64588b97b10d7
zevarix <12117700+zevarix@users.noreply.github.com>
2026-08-27 17:36:58 -0700

    Fix Homebrew audit metadata

--- Formula/pantheon-local-tools.rb
+++ Formula/pantheon-local-tools.rb
@@ -2,11 +2,9 @@ class PantheonLocalTools < Formula
   desc "Provider-neutral local development helpers for Pantheon"
   homepage "https://github.com/zevarix/pantheon-local-tools"
   url "https://github.com/zevarix/pantheon-local-tools/releases/download/v0.1.0/pantheon-local-tools-0.1.0.tar.gz"
-  version "0.1.0"
   sha256 "c4d9848d032918536eb357058ea36e4ab19ad37e76cf7d0f52c830f876e44161"
   license "MIT"
 
-  uses_from_macos "git"
 
   def install
     libexec.install "bin", "libexec", "VERSION", "LICENSE", "README.md"
