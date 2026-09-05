269ca5dac2c935493351d7893edbc9c3c74d4fd5
Peder Klockmann <12117700+zevarix@users.noreply.github.com>
2026-09-04 17:25:29 -0700

    Update Pantheon Local Tools to v0.1.2

--- Formula/pantheon-local-tools.rb
+++ Formula/pantheon-local-tools.rb
@@ -1,8 +1,8 @@
 class PantheonLocalTools < Formula
   desc "Provider-neutral local development helpers for Pantheon"
   homepage "https://github.com/zevarix/pantheon-local-tools"
-  url "https://github.com/zevarix/pantheon-local-tools/releases/download/v0.1.1/pantheon-local-tools-0.1.1.tar.gz"
-  sha256 "a85e14adedb98895c893f02f400dd5168c760cfc05f450c69c438ca95c047f19"
+  url "https://github.com/zevarix/pantheon-local-tools/releases/download/v0.1.2/pantheon-local-tools-0.1.2.tar.gz"
+  sha256 "f70a47e7b88a7219cfb44ac0cfec93f3e1f5ad33a51db7bd9cfb0e944f301170"
   license "MIT"
 
   def install
